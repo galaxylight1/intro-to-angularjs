725c12342ad05f59febf9f37df3d574ceef05cab
prateek <prateeksingh8841@gmail.com>
2022-12-26 15:44:41 -0700

    test

--- app/app.js
+++ app/app.js
@@ -24,6 +24,7 @@ myNinjaApp.directive('randomNinja', [function() {
             title: '='
         },
         templateUrl: 'views/random.html',
+        transclude: true, 
         controller: function($scope) {
             $scope.random = Math.floor(Math.random() * 4);
         }
