14db851151a467391f7edfa8797a9a9b00e8f5bd
prateek <prateeksingh8841@gmail.com>
2022-12-26 12:18:12 -0700

    test

---
+++ app/app.js
@@ -0,0 +1,56 @@
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+
+myNinjaApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html',
+            controller: 'NinjaController'
+        })
+        .when('/directory', {
+            templateUrl: 'views/directory.html',
+            controller: 'NinjaController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        })
+}]);
+
+myNinjaApp.directive('randomNinja', [function() {
+    
+    return {
+        restrict: 'EA',
+        scope: {
+            ninjas: '=',
+            title: '='
+        },
+        templateUrl: 'views/random.html',
+        controller: function($scope) {
+            $scope.random = Math.floor(Math.random() * 4);
+        }
+    };
+
+}]);
+
+myNinjaApp.controller('NinjaController', ['$scope', '$http', function($scope, $http) {
+    $scope.removeNinja = function(ninja) {
+        var removeNinja = $scope.ninjas.indexOf(ninja);
+        $scope.ninjas.splice(removeNinja, 1);
+    }
+
+    $scope.addNinja = function(ninja) {
+        $scope.ninjas.push({
+            name: $scope.newninja.name,
+            belt: $scope.newninja.belt,
+            rate: parseInt($scope.newninja.rate),
+            available: true
+        });
+
+        $scope.newninja.name = '';
+        $scope.newninja.belt = '';
+        $scope.newninja.rate = '';2
+    }
+
+    $http.get('data/ninjas.json').then(function(response) {
+        $scope.ninjas = response.data;
+    });
+}]);
